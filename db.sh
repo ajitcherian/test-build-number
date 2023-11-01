dc2abe6c5856b3418505d3c02e05ee574a2e6c9d
ajitcherian <54443253+ajitcherian@users.noreply.github.com>
2023-11-01 22:39:10 +0530

    Create db.sh

---
+++ db.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Function to run SQL scripts
+runSQL() {
+    #set -ex
+    isGetCount=true
+    echo "hello"
+    #local result=$?
+    if [ "$isGetCount" == "true" ]; then
+        echo "$result"
+    else
+        if [ "$?" -eq 0 ]; then
+            echo "Success"
+        else
+            echo "Failed"
+        fi
+    fi
+}
+
+count=`runSQL $db_update_verify_sql_path true`
+if [ ${count} == '0' ]; then
+  echo "Hello Count 0"
+  local isSuccess
+fi  
