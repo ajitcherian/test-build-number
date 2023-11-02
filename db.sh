25f90a89b290daf4edb5a89450be480d271acdb9
ajitcherian <54443253+ajitcherian@users.noreply.github.com>
2023-11-02 11:10:56 +0530

    Update db.sh

--- db.sh
+++ db.sh
@@ -3,17 +3,18 @@
 runSQL() {
     set -ex
     isGetCount=true
-    ls -la
-    echo $?
-    return $?
+    echo "hello"
+    local result=$?
+    return result
 }
 
-runSQL $db_update_verify_sql_path true
-echo $?
-count=$?
-echo "Exit code: $count"
-count=$(echo ${count} | sed 's/ //g')
-echo "Modified exit code: $count"
-if [ $count -eq 0 ]; then
-  echo "Hello Count 0"
-fi
+count=$(runSQL $db_update_verify_sql_path true)
+
+echo "t***REMOVED***s is return value $count"
+#$count=$?
+#echo "Exit code: $count"
+#count=$(echo ${count} | sed 's/ //g')
+#echo "Modified exit code: $count"
+#if [ $count -eq 0 ]; then
+#  echo "Hello Count 0"
+#fi
